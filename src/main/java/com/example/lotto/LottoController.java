0b0d00b9c4f04ec8e8365a67c874650a75ca7dd5
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-08 15:32:59 +0100

    Add 5 limit to list and print random numbers

--- src/main/java/com/example/lotto/LottoController.java
+++ src/main/java/com/example/lotto/LottoController.java
@@ -6,14 +6,35 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LottoController {
     @FXML
     private Button sorsolGomb;
     @FXML
-    private Label sorsoltLabel;
+    private Label sorsoltSzamLabel;
     @FXML
-    private HBox sorosoltSzamok;
+    private HBox sorsoltSzamok;
+    private static final Random rng = new Random();
+    private List<Integer> sorsoltSzamokLista = new ArrayList<>();
 
+    @FXML
     public void sorsolCLick(ActionEvent actionEvent) {
+        if(sorsoltSzamokLista.size()==5){
+            return;
+        }
+        int sorsoltSzam = rng.nextInt(98)+1;
+        while (sorsoltSzamokLista.contains(sorsoltSzam)){
+            sorsoltSzam = rng.nextInt(98)+1;
+        }
+        sorsoltSzamokLista.add(sorsoltSzam);
+        sorsoltSzamLabel.setText(String.valueOf(sorsoltSzam));
+
+        sorsoltSzamok.getChildren().clear();
+        for (int szam: sorsoltSzamokLista){
+            sorsoltSzamok.getChildren().add(new Label(String.valueOf(szam)));
+        }
     }
 }
