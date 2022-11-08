8112159cfbfa656928d1005ce8437f951b7e8781
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-08 15:35:57 +0100

    Add sort button when list reaches 5 length

--- src/main/java/com/example/lotto/LottoController.java
+++ src/main/java/com/example/lotto/LottoController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -23,18 +24,27 @@ public class LottoController {
     @FXML
     public void sorsolCLick(ActionEvent actionEvent) {
         if(sorsoltSzamokLista.size()==5){
-            return;
-        }
-        int sorsoltSzam = rng.nextInt(98)+1;
-        while (sorsoltSzamokLista.contains(sorsoltSzam)){
-            sorsoltSzam = rng.nextInt(98)+1;
-        }
-        sorsoltSzamokLista.add(sorsoltSzam);
-        sorsoltSzamLabel.setText(String.valueOf(sorsoltSzam));
+            Collections.sort(sorsoltSzamokLista);
+            sorsoltSzamok.getChildren().clear();
+            for (int szam: sorsoltSzamokLista){
+                sorsoltSzamok.getChildren().add(new Label(String.valueOf(szam)));
+            }
+        }else {
+            int sorsoltSzam = rng.nextInt(98)+1;
+            while (sorsoltSzamokLista.contains(sorsoltSzam)){
+                sorsoltSzam = rng.nextInt(98)+1;
+            }
+            sorsoltSzamokLista.add(sorsoltSzam);
+            sorsoltSzamLabel.setText(String.valueOf(sorsoltSzam));
 
-        sorsoltSzamok.getChildren().clear();
-        for (int szam: sorsoltSzamokLista){
-            sorsoltSzamok.getChildren().add(new Label(String.valueOf(szam)));
+            sorsoltSzamok.getChildren().clear();
+            for (int szam: sorsoltSzamokLista){
+                sorsoltSzamok.getChildren().add(new Label(String.valueOf(szam)));
+            }
+        }
+
+        if(sorsoltSzamokLista.size()==5){
+            sorsolGomb.setText("Rendez");
         }
     }
 }
