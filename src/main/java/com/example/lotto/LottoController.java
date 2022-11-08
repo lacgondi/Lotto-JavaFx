582c08851e492ba69b8606a0ba0237e236b055f1
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-08 15:19:26 +0100

    First commit

---
+++ src/main/java/com/example/lotto/LottoController.java
@@ -0,0 +1,19 @@
+package com.example.lotto;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class LottoController {
+    @FXML
+    private Button sorsolGomb;
+    @FXML
+    private Label sorsoltLabel;
+    @FXML
+    private HBox sorosoltSzamok;
+
+    public void sorsolCLick(ActionEvent actionEvent) {
+    }
+}
